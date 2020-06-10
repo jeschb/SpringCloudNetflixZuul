1929bdc8bfc12fa990e8f74c2f7c729bec9686ad
Chuss <jeschb@outlook.com>
2020-06-09 23:32:56 -0500

    Add files via upload

---
+++ src/test/java/com/jchavez/zuul/SpringCloudNetflixZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jchavez.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudNetflixZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
